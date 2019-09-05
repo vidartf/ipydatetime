8764d7d3488645fcdcc20f59c64997fa56f6f408
Vidar Tonaas Fauske <vidartf@gmail.com>
2019-09-05 16:54:38 +0100

    black

--- setup.py
+++ setup.py
@@ -11,9 +11,13 @@ from os.path import join as pjoin
 
 
 from jupyter_packaging import (
-    create_cmdclass, install_npm, ensure_targets,
-    find_packages, combine_commands, ensure_python,
-    get_version
+    create_cmdclass,
+    install_npm,
+    ensure_targets,
+    find_packages,
+    combine_commands,
+    ensure_python,
+    get_version,
 )
 
 from setuptools import setup
@@ -22,98 +26,82 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 # The name of the project
-name = 'ipydatetime'
+name = "ipydatetime"
 
 # Ensure a valid python version
-ensure_python('>=3.5')
+ensure_python(">=3.5")
 
 # Get our version
-version = get_version(pjoin(name, '_version.py'))
+version = get_version(pjoin(name, "_version.py"))
 
-nb_path = pjoin(HERE, name, 'nbextension', 'static')
-js_path = pjoin(HERE, 'ts')
-lab_path = pjoin(HERE, name, 'labextension')
+nb_path = pjoin(HERE, name, "nbextension", "static")
+js_path = pjoin(HERE, "ts")
+lab_path = pjoin(HERE, name, "labextension")
 
 # Representative files that should exist after a successful build
-jstargets = [
-    pjoin(nb_path, 'index.js'),
-    pjoin(js_path, 'lib', 'plugin.js'),
-]
+jstargets = [pjoin(nb_path, "index.js"), pjoin(js_path, "lib", "plugin.js")]
 
-package_data_spec = {
-    name: [
-        'nbextension/static/*.*js*',
-        'labextension/*.tgz'
-    ]
-}
+package_data_spec = {name: ["nbextension/static/*.*js*", "labextension/*.tgz"]}
 
 data_files_spec = [
-    ('share/jupyter/nbextensions/ipydatetime',
-        nb_path, '*.js*'),
-    ('share/jupyter/lab/extensions', lab_path, '*.tgz'),
-    ('etc/jupyter/nbconfig/notebook.d' , HERE, 'ipydatetime.json')
+    ("share/jupyter/nbextensions/ipydatetime", nb_path, "*.js*"),
+    ("share/jupyter/lab/extensions", lab_path, "*.tgz"),
+    ("etc/jupyter/nbconfig/notebook.d", HERE, "ipydatetime.json"),
 ]
 
 
-cmdclass = create_cmdclass('jsdeps', package_data_spec=package_data_spec,
-    data_files_spec=data_files_spec)
-cmdclass['jsdeps'] = combine_commands(
-    install_npm(js_path, build_cmd='build:all'),
-    ensure_targets(jstargets),
+cmdclass = create_cmdclass(
+    "jsdeps", package_data_spec=package_data_spec, data_files_spec=data_files_spec
+)
+cmdclass["jsdeps"] = combine_commands(
+    install_npm(js_path, build_cmd="build:all"), ensure_targets(jstargets)
 )
 
 
 setup_args = dict(
-    name            = name,
-    description     = 'A Jupyter Widget library for time and datetime pickers',
-    version         = version,
-    scripts         = glob(pjoin('scripts', '*')),
-    cmdclass        = cmdclass,
-    packages        = find_packages(HERE),
-    author          = 'Vidar Tonaas Fauske',
-    author_email    = 'vidartf@gmail.com',
-    url             = 'https://github.com/vidartf/ipydatetime',
-    license         = 'BSD',
-    platforms       = "Linux, Mac OS X, Windows",
-    keywords        = ['Jupyter', 'Widgets', 'IPython'],
-    classifiers     = [
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Framework :: Jupyter',
+    name=name,
+    description="A Jupyter Widget library for time and datetime pickers",
+    version=version,
+    scripts=glob(pjoin("scripts", "*")),
+    cmdclass=cmdclass,
+    packages=find_packages(HERE),
+    author="Vidar Tonaas Fauske",
+    author_email="vidartf@gmail.com",
+    url="https://github.com/vidartf/ipydatetime",
+    license="BSD",
+    platforms="Linux, Mac OS X, Windows",
+    keywords=["Jupyter", "Widgets", "IPython"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Framework :: Jupyter",
     ],
-    include_package_data = True,
-    install_requires = [
-        'ipywidgets>=7.0.0',
-    ],
-    extras_require = {
-        'test': [
-            'pytest>=3.6',
-            'pytest-cov',
-            'nbval',
-        ],
-        'examples': [
+    include_package_data=True,
+    install_requires=["ipywidgets>=7.0.0"],
+    extras_require={
+        "test": ["pytest>=3.6", "pytest-cov", "nbval"],
+        "examples": [
             # Any requirements for the examples to run
         ],
-        'docs': [
-            'sphinx>=1.5',
-            'recommonmark',
-            'sphinx_rtd_theme',
-            'nbsphinx>=0.2.13,<0.4.0',
-            'jupyter_sphinx',
-            'nbsphinx-link',
-            'pytest_check_links',
-            'pypandoc',
+        "docs": [
+            "sphinx>=1.5",
+            "recommonmark",
+            "sphinx_rtd_theme",
+            "nbsphinx>=0.2.13,<0.4.0",
+            "jupyter_sphinx",
+            "nbsphinx-link",
+            "pytest_check_links",
+            "pypandoc",
         ],
     },
-    entry_points = {
-    },
+    entry_points={},
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(**setup_args)
