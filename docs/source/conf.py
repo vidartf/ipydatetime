8dc6f0cbe89e9fcdf4cf285ff4d9c4d24faa3a46
Vidar Tonaas Fauske <vidartf@gmail.com>
2019-09-09 14:11:29 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -200,8 +200,6 @@ nbsphinx_allow_errors = True  # exception ipstruct.py ipython_genutils
 
 
 def setup(app):
-    app.setup_extension("jupyter_sphinx.embed_widgets")
-
     def add_scripts(app):
         for fname in ["helper.js", "embed-bundle.js"]:
             if not os.path.exists(os.path.join(here, "_static", fname)):
